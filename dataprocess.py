b688f9d378cd4c12b05a9245f2409a879e4d2ed5
IkiHiyori <1002877803@qq.com>
2019-01-09 16:59:35 +0800

    Update dataprocess.py

--- dataprocess.py
+++ dataprocess.py
@@ -4,25 +4,49 @@ from scipy.sparse import csr_matrix
 import time
 
 
-def get_data(data_path):
+def get_data_csr(train_path, test_path):
+    train, train_labels = read_data(train_path)
+    train_labels_csr = labels_to_csr(train_labels, csr_shape=(len(train), 798))  # 0~797
+    test, test_labels = read_data(test_path)
+    test_labels_csr = labels_to_csr(test_labels, csr_shape=(len(test), 798))
+    tfidf_train = TfidfVectorizer(analyzer=lambda x: x.split(' '))
+    train_csr = tfidf_train.fit_transform(train)  # csr matrix
+    tfidf_test = TfidfVectorizer(vocabulary=tfidf_train.vocabulary_)
+    test_csr = tfidf_test.fit_transform(test)
+    return train_csr, train_labels_csr, test_csr, test_labels_csr
+
+
+def read_data(path):
     data = []
     labels = []
-    with open(data_path, 'r') as f:
+    with open(path, 'r') as f:
         for line in f.readlines():
             data_dic = json.loads(line)  # keys=['id', 'title', 'abstract', 'section', 'subsection', 'group', 'labels']
             str = ' '
             data.append(str.join(data_dic['abstract']))
             labels.append(data_dic['labels'])
-    tfidfVecorizer = TfidfVectorizer(analyzer=lambda x: x.split(' '))
-    tf_idf_csr = tfidfVecorizer.fit_transform(data)  # csr matrix
+    return data, labels
+
+
+def labels_to_csr(raw_labels, csr_shape):
     # labels to csr matrix
     row = []
     col = []
-    for (i, label) in enumerate(labels):
+    for (i, label) in enumerate(raw_labels):
         for count in range(len(label)):
             row.append(i)
         for j in label:
             col.append(j)
     ones = [1] * len(row)
-    labels_csr = csr_matrix((ones, (row, col)), shape=(len(data), 798))
-    return tf_idf_csr, labels_csr
+    labels_csr = csr_matrix((ones, (row, col)), shape=csr_shape)
+    return labels_csr
+
+
+if __name__ == '__main__':
+    start = time.clock()
+    train_path = 'dataset/train.json'
+    test_path = 'dataset/test.json'
+    train_csr, train_labels_csr, test_csr, test_labels_csr = get_data_csr(train_path, test_path)
+    end = time.clock()
+    print('time: {: .2f} s'.format(end - start))
+
